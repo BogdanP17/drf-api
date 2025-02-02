bd40999b524c585852711744afc104338bdbac5e
BogdanP17 <petrutescubogdan@yahoo.com>
2025-02-02 04:32:42 +0000

    add the json packeges

--- followers/views.py
+++ followers/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
+from drf_api.permission import IsOwnerOrReadOnly
 from .models import Follower
-from .serializers import FollowerSerializer
+from .serializer import FollowerSerializer
 
 
 class FollowerList(generics.ListCreateAPIView):
