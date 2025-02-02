abdf6171d035ca5c806c6614d586dbaa903bffd5
BogdanP17 <petrutescubogdan@yahoo.com>
2025-02-02 00:14:24 +0000

    added profile serializer and list view

---
+++ profiles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+
+    class Meta:
+        model = Profile
+        field = [
+            'id', 'owner', 'created_', 'updated_at', 'name',
+            'content', 'image'
+        ]
